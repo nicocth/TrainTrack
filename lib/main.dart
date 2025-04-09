f87d6fd72df8214401f9635272eff55f564b630e
nicocth <nikokt27@gmail.com>
2025-04-09 13:16:56 +0200

    Add Firebase Crashlytics integration and update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:train_track/presentation/my_app.dart';
@@ -6,10 +7,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+// Initialize Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Catch uncaught errors and send them to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
   runApp(const ProviderScope(child: MyApp()));
 }
-
-
